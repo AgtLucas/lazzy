086d7ca94dfe1dbbb251173eb32406325b906121
Lucas <lucas@agtlucas.com>
2016-02-24 21:34:06 -0300

    JS Standard

--- shared/routes.js
+++ shared/routes.js
@@ -1,15 +1,15 @@
 /* eslint no-unused-vars:0 */
-import { Route, IndexRoute } from 'react-router';
-import React from 'react';
-import App from './container/App';
-import PostContainer from './container/PostContainer/PostContainer';
-import PostDetailView from './container/PostDetailView/PostDetailView';
+import { Route, IndexRoute } from 'react-router'
+import React from 'react'
+import App from './container/App'
+import PostContainer from './container/PostContainer/PostContainer'
+import PostDetailView from './container/PostDetailView/PostDetailView'
 
 const routes = (
-  <Route path="/" component={App} >
+  <Route path='/' component={App} >
     <IndexRoute component={PostContainer} />
-    <Route path="/post/:slug" component={PostDetailView}/>
+    <Route path='/post/:slug' component={PostDetailView}/>
   </Route>
-);
+)
 
-export default routes;
+export default routes
