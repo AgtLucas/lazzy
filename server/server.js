b9093935f16c0bba58c40f0effd02a8ce8ea0de6
Lucas <lucas@agtlucas.com>
2016-02-24 21:30:59 -0300

    JS Standard

--- server/server.js
+++ server/server.js
@@ -1,57 +1,57 @@
-import Express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import path from 'path';
+import Express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import path from 'path'
 
 // Webpack Requirements
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpack from 'webpack'
+import config from '../webpack.config.dev'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 
 // Initialize the Express App
-const app = new Express();
+const app = new Express()
 
 if (process.env.NODE_ENV !== 'production') {
-  const compiler = webpack(config);
-  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-  app.use(webpackHotMiddleware(compiler));
+  const compiler = webpack(config)
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+  app.use(webpackHotMiddleware(compiler))
 }
 
 // React And Redux Setup
-import { configureStore } from '../shared/redux/store/configureStore';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
+import { configureStore } from '../shared/redux/store/configureStore'
+import { Provider } from 'react-redux'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
 
 // Import required modules
-import routes from '../shared/routes';
-import { fetchComponentData } from './util/fetchData';
-import posts from './routes/post.routes';
-import dummyData from './dummyData';
-import serverConfig from './config';
+import routes from '../shared/routes'
+import { fetchComponentData } from './util/fetchData'
+import posts from './routes/post.routes'
+import dummyData from './dummyData'
+import serverConfig from './config'
 
 // MongoDB Connection
 mongoose.connect(serverConfig.mongoURL, (error) => {
   if (error) {
-    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-    throw error;
+    console.error('Please make sure Mongodb is installed and running!') // eslint-disable-line no-console
+    throw error
   }
 
   // feed some dummy data in DB.
-  dummyData();
-});
+  dummyData()
+})
 
 // Apply body Parser and server public assets and routes
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
-app.use(Express.static(path.resolve(__dirname, '../static')));
-app.use('/api', posts);
+app.use(bodyParser.json({ limit: '20mb' }))
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }))
+app.use(Express.static(path.resolve(__dirname, '../static')))
+app.use('/api', posts)
 
 // Render Initial HTML
 const renderFullPage = (html, initialState) => {
-  const cssPath = process.env.NODE_ENV === 'production' ? '/css/app.min.css' : '/css/app.css';
+  const cssPath = process.env.NODE_ENV === 'production' ? '/css/app.min.css' : '/css/app.css'
   return `
     <!doctype html>
     <html>
@@ -67,28 +67,28 @@ const renderFullPage = (html, initialState) => {
       <body>
         <div id="root">${html}</div>
         <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
         </script>
         <script src="/dist/bundle.js"></script>
       </body>
     </html>
-  `;
-};
+  `
+}
 
 // Server Side Rendering based on routes matched by React-router.
 app.use((req, res) => {
   match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
     if (err) {
-      return res.status(500).end('Internal server error');
+      return res.status(500).end('Internal server error')
     }
 
     if (!renderProps) {
-      return res.status(404).end('Not found!');
+      return res.status(404).end('Not found!')
     }
 
-    const initialState = { posts: [], post: {} };
+    const initialState = { posts: [], post: {} }
 
-    const store = configureStore(initialState);
+    const store = configureStore(initialState)
 
     fetchComponentData(store.dispatch, renderProps.components, renderProps.params)
       .then(() => {
@@ -96,22 +96,22 @@ app.use((req, res) => {
           <Provider store={store}>
             <RouterContext {...renderProps} />
           </Provider>
-        );
-        const finalState = store.getState();
+        )
+        const finalState = store.getState()
 
-        res.status(200).end(renderFullPage(initialView, finalState));
+        res.status(200).end(renderFullPage(initialView, finalState))
       })
       .catch(() => {
-        res.end(renderFullPage('Error', {}));
-      });
-  });
-});
+        res.end(renderFullPage('Error', {}))
+      })
+  })
+})
 
 // start app
 app.listen(serverConfig.port, (error) => {
   if (!error) {
-    console.log(`MERN is running on port: ${serverConfig.port}! Build something amazing!`); // eslint-disable-line
+    console.log(`MERN is running on port: ${serverConfig.port}! Build something amazing!`) // eslint-disable-line
   }
-});
+})
 
-export default app;
+export default app
