fde2eb7d31f49b0d4b773a010808c80b6002d9c6
Lucas <lucas@agtlucas.com>
2016-02-24 21:33:31 -0300

    JS Standard

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,59 +1,59 @@
-import Post from '../models/post';
-import cuid from 'cuid';
-import slug from 'slug';
-import sanitizeHtml from 'sanitize-html';
+import Post from '../models/post'
+import cuid from 'cuid'
+import slug from 'slug'
+import sanitizeHtml from 'sanitize-html'
 
-export function getPosts(req, res) {
+export function getPosts (req, res) {
   Post.find().sort('-dateAdded').exec((err, posts) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).send(err)
     }
-    res.json({ posts });
-  });
+    res.json({ posts })
+  })
 }
 
-export function addPost(req, res) {
+export function addPost (req, res) {
   if (!req.body.post.name || !req.body.post.title || !req.body.post.content) {
-    return res.status(403).end();
+    return res.status(403).end()
   }
 
-  const newPost = new Post(req.body.post);
+  const newPost = new Post(req.body.post)
 
   // Let's sanitize inputs
-  newPost.title = sanitizeHtml(newPost.title);
-  newPost.name = sanitizeHtml(newPost.name);
-  newPost.content = sanitizeHtml(newPost.content);
+  newPost.title = sanitizeHtml(newPost.title)
+  newPost.name = sanitizeHtml(newPost.name)
+  newPost.content = sanitizeHtml(newPost.content)
 
-  newPost.slug = slug(newPost.title.toLowerCase(), { lowercase: true });
-  newPost.cuid = cuid();
+  newPost.slug = slug(newPost.title.toLowerCase(), { lowercase: true })
+  newPost.cuid = cuid()
   newPost.save((err, saved) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).send(err)
     }
-    return res.json({ post: saved });
-  });
+    return res.json({ post: saved })
+  })
 }
 
-export function getPost(req, res) {
-  const newSlug = req.query.slug.split('-');
-  const newCuid = newSlug[newSlug.length - 1];
+export function getPost (req, res) {
+  const newSlug = req.query.slug.split('-')
+  const newCuid = newSlug[newSlug.length - 1]
   Post.findOne({ cuid: newCuid }).exec((err, post) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).send(err)
     }
-    res.json({ post });
-  });
+    res.json({ post })
+  })
 }
 
-export function deletePost(req, res) {
-  const postId = req.body.postId;
+export function deletePost (req, res) {
+  const postId = req.body.postId
   Post.findById(postId).exec((err, post) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).send(err)
     }
 
     post.remove(() => {
-      res.status(200).end();
-    });
-  });
+      res.status(200).end()
+    })
+  })
 }
